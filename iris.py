8ed96458cc64dc9b387d0dd174a021a1f5ab9b7c
Simon Hua <simonhua_dragon@hotmail.com>
2018-09-30 17:58:58 -0400

    Create iris.py

---
+++ iris.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+iris_dataset = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv'
+
+iris = np.recfromcsv(iris_dataset, encoding=None)
+iris = pd.read_csv(iris_dataset)
+
+iris_setosa = iris['species'] == "setosa"
+iris_versicolor = iris['species'] == "versicolor"
+iris_virginica = iris['species'] == "virginica"
+
+print(iris[iris_setosa].mean())
+print(iris[iris_versicolor].mean())
+print(iris[iris_virginica].mean())
+
+print(iris[iris_setosa].std())
+print(iris[iris_versicolor].std())
+print(iris[iris_virginica].std())
